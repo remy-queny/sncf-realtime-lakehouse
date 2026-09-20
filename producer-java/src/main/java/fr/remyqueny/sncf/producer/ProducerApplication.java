4d74caa42d26591523995a87e5851e24a95379f5
Remy Queny <remy.queny@etu.isima.fr>
2026-09-20 18:52:07 +0200

    initialisation kafka producer module

---
+++ producer-java/src/main/java/fr/remyqueny/sncf/producer/ProducerApplication.java
@@ -0,0 +1,11 @@
+package fr.remyqueny.sncf.producer;
+
+public final class ProducerApplication {
+
+    private ProducerApplication() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("SNCF Kafka producer bootstrap complete.");
+    }
+}
